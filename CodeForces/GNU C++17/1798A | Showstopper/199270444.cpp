b51a08e4ca81d21eb03ec7ff5a121413c798e025
PranavTam <136703398+PranavTam@users.noreply.github.com>
2023-06-15 16:28:26 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1798A | Showstopper/199270444.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define pb push_back
+#define mp make_pair
+# define endl "\n"
+ 
+#define f0r(i,n) for(int i=0;i<n;i++)
+ 
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b)  (a/gcd(a,b))*b
+ 
+int32_t main(){ 
+    
+ios_base::sync_with_stdio(false);
+cin.tie(NULL); 
+cout.tie(NULL); 
+ 
+int t;
+cin>>t;
+while(t--){  
+int n;
+cin>>n;
+vector<int>a(n),b(n);
+for(int i=0;i<n;i++)
+{
+    cin>>a[i];
+ 
+}
+for(int i=0;i<n;i++)
+{
+    cin>>b[i];
+ 
+}
+int m1=0,m2=0;
+ int ma1,ma2;
+ 
+for(int i=0;i<n;i++)
+{
+ 
+if(a[i]>b[i])swap(a[i],b[i]);
+}
+ma1=*max_element(a.begin(),a.end());
+ma2=*max_element(b.begin(),b.end());
+ if((ma1==a[n-1]) && (ma2==b[n-1]))cout<<"YES"<<endl;
+else cout<<"NO"<<endl;
+ 
+} 
+ 
+}
+        
+ 
+        
+ 
