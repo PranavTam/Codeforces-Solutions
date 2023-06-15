3f296b8160f83dc8b849fb1f6baad1d559a0daac
PranavTam <136703398+PranavTam@users.noreply.github.com>
2023-06-15 16:28:42 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1807A | Plus or Minus/198116624.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define pb push_back
+#define mp make_pair
+# define endl "\n"
+ 
+#define f0r(i,n) for(int i=0;i<n;i++)
+ 
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b)  (a/gcd(a,b))*b
+ 
+int32_t main(){ 
+    
+ios_base::sync_with_stdio(false);
+cin.tie(NULL); 
+cout.tie(NULL); 
+ 
+int t;
+cin>>t;
+while(t--){  
+int a,b,c;
+cin>>a>>b>>c;
+if(a+b==c)cout<<'+'<<endl;
+else cout<<'-'<<endl;
+ 
+} 
+ 
+}
+        
+ 
+        
+ 
