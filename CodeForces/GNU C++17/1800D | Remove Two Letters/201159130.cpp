bd45ea6dea43dd82c830d2d6c7056b2165e789f2
PranavTam <136703398+PranavTam@users.noreply.github.com>
2023-06-15 16:18:46 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1800D | Remove Two Letters/201159130.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define pb push_back
+#define mp make_pair
+# define endl "\n"
+ 
+#define f0r(i,n) for(int i=0;i<n;i++)
+ 
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b)  (a/gcd(a,b))*b
+ 
+int32_t main(){ 
+    
+ios_base::sync_with_stdio(false);
+cin.tie(NULL); 
+cout.tie(NULL); 
+ 
+int t;
+cin>>t;
+while(t--){  
+int n,a=0;
+cin>>n;
+string s;
+cin>>s;
+ 
+for(int i=0;i<n-1;i++)
+{
+    if(s[i]!=s[i+2])a++;
+}
+cout<<a<<endl;
+ 
+} 
+ 
+}
+        
+ 
+        
+ 
