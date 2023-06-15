ec53d7e0cbf15643c1ddf61e3d0dd3bd160968bc
PranavTam <136703398+PranavTam@users.noreply.github.com>
2023-06-15 16:18:09 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1828B | Permutation Swap/208173704.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define pb push_back
+#define mp make_pair
+# define endl "\n"
+ 
+#define f0r(i,n) for(int i=0;i<n;i++)
+ 
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b)  (a/gcd(a,b))*b
+ 
+int32_t main(){ 
+    
+ios_base::sync_with_stdio(false);
+cin.tie(NULL); 
+cout.tie(NULL); 
+ 
+int t;
+cin>>t;
+while(t--){  
+int n;
+cin>>n;
+vector<int>a(n);
+int ans=0;
+for(int i=0;i<n;i++)
+{
+  cin>>a[i];
+  ans=gcd(ans,abs(a[i]-(i+1)));
+   
+}
+cout<<ans<<endl;
+ 
+ 
+} 
+ 
+}
+        
+ 
+        
+ 
