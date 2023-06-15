02a0050e900f1b1d396c96301384a0f5f88d53f8
PranavTam <136703398+PranavTam@users.noreply.github.com>
2023-06-15 16:28:45 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1794B | Not Dividing/196024636.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+ 
+int main(){ 
+    
+ios_base::sync_with_stdio(false);
+cin.tie(NULL); 
+cout.tie(NULL); 
+ 
+int t;
+cin>>t;
+while(t--){  
+int n;
+cin>>n;
+vector<int>v(n);
+for(int i=0;i<n;i++)
+{
+    cin>>v[i];
+   if(v[i]==1)v[i]++;
+}
+for(int i=0;i<n-1;i++)
+{
+   if(v[i+1]%v[i]==0)v[i+1]++;
+}
+for(int i=0;i<n;i++)
+{
+    cout<<v[i]<<" ";
+}
+cout<<endl;
+ 
+} 
+ 
+}
+        
+ 
+        
+ 
