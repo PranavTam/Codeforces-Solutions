2ea97f8dcb29f0b2d76a7ca0b54074eaf662b0e4
PranavTam <136703398+PranavTam@users.noreply.github.com>
2023-06-15 16:18:53 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1758A | SSeeeeiinngg DDoouubbllee/201120545.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define pb push_back
+#define mp make_pair
+# define endl "\n"
+ 
+#define f0r(i,n) for(int i=0;i<n;i++)
+ 
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b)  (a/gcd(a,b))*b
+ 
+int32_t main(){ 
+    
+ios_base::sync_with_stdio(false);
+cin.tie(NULL); 
+cout.tie(NULL); 
+ 
+int t;
+cin>>t;
+while(t--){  
+string s,t;
+cin>>s;
+t=s;
+reverse(t.begin(),t.end());
+s+=t;
+cout<<s<<endl;
+ 
+ 
+} 
+ 
+}
+        
+ 
+        
+ 
